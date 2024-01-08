74a65e3f283d556aa4a340e95d2f18de8faf18b6
Bubbles <hxhbubble@gmail.com>
2024-01-08 08:25:04 +0530

    Components and depend added

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { AArrowDown } from "lucide-react";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <AArrowDown />
+    </div>
+  );
+};
+
+export default Home;
